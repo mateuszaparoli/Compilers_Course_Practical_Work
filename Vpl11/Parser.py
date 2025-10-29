d310b2420e10733f00a2c31f355e012316630cb3
mateuszaparoli <mateuszaparoli7@gmail.com>
2025-10-28 23:32:17 -0300

    Implement Lexer, Parser, and Visitor for arithmetic expressions
    - Added Lexer class to tokenize input strings, supporting various token types including numbers, variables, and operators.
    - Created Parser class to parse the token stream into an abstract syntax tree (AST) for arithmetic expressions, handling expressions, comparisons, and let bindings.
    - Developed Visitor pattern with GenVisitor class to generate low-level assembly code from the AST.
    - Included driver script to read input, invoke the lexer and parser, and execute the generated program.
    - Added unit tests in docstrings for Lexer, Parser, and Visitor methods to ensure correctness of implementation.

---
+++ Vpl11/Parser.py
@@ -0,0 +1,349 @@
+import sys
+
+from Expression import *
+from Lexer import Token, TokenType
+
+"""
+This file implements the parser of arithmetic expressions. The same rules of
+precedence and associativity from Lab 5: Visitors, apply.
+
+References:
+    see https://www.engr.mun.ca/~theo/Misc/exp_parsing.htm#classic
+"""
+
+class Parser:
+    def __init__(self, tokens):
+        """
+        Initializes the parser. The parser keeps track of the list of tokens
+        and the current token. For instance:
+        """
+        self.tokens = list(tokens)
+        self.tokens.append(Token("", TokenType.EOF))
+        self.cur_token_idx = 0
+
+    def parse(self):
+        """
+        Returns the expression associated with the stream of tokens.
+
+        Examples:
+        >>> parser = Parser([Token('123', TokenType.NUM)])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        123
+
+        >>> parser = Parser([Token('True', TokenType.TRU)])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        1
+
+        >>> parser = Parser([Token('False', TokenType.FLS)])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        0
+
+        >>> tk0 = Token('~', TokenType.NEG)
+        >>> tk1 = Token('123', TokenType.NUM)
+        >>> parser = Parser([tk0, tk1])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        -123
+
+        >>> tk0 = Token('3', TokenType.NUM)
+        >>> tk1 = Token('*', TokenType.MUL)
+        >>> tk2 = Token('4', TokenType.NUM)
+        >>> parser = Parser([tk0, tk1, tk2])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        12
+
+        >>> tk0 = Token('3', TokenType.NUM)
+        >>> tk1 = Token('*', TokenType.MUL)
+        >>> tk2 = Token('~', TokenType.NEG)
+        >>> tk3 = Token('4', TokenType.NUM)
+        >>> parser = Parser([tk0, tk1, tk2, tk3])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        -12
+
+        >>> tk0 = Token('30', TokenType.NUM)
+        >>> tk1 = Token('/', TokenType.DIV)
+        >>> tk2 = Token('4', TokenType.NUM)
+        >>> parser = Parser([tk0, tk1, tk2])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        7
+
+        >>> tk0 = Token('3', TokenType.NUM)
+        >>> tk1 = Token('+', TokenType.ADD)
+        >>> tk2 = Token('4', TokenType.NUM)
+        >>> parser = Parser([tk0, tk1, tk2])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        7
+
+        >>> tk0 = Token('30', TokenType.NUM)
+        >>> tk1 = Token('-', TokenType.SUB)
+        >>> tk2 = Token('4', TokenType.NUM)
+        >>> parser = Parser([tk0, tk1, tk2])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        26
+
+        >>> tk0 = Token('2', TokenType.NUM)
+        >>> tk1 = Token('*', TokenType.MUL)
+        >>> tk2 = Token('(', TokenType.LPR)
+        >>> tk3 = Token('3', TokenType.NUM)
+        >>> tk4 = Token('+', TokenType.ADD)
+        >>> tk5 = Token('4', TokenType.NUM)
+        >>> tk6 = Token(')', TokenType.RPR)
+        >>> parser = Parser([tk0, tk1, tk2, tk3, tk4, tk5, tk6])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        14
+
+        >>> tk0 = Token('4', TokenType.NUM)
+        >>> tk1 = Token('==', TokenType.EQL)
+        >>> tk2 = Token('4', TokenType.NUM)
+        >>> parser = Parser([tk0, tk1, tk2])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        1
+
+        >>> tk0 = Token('4', TokenType.NUM)
+        >>> tk1 = Token('<=', TokenType.LEQ)
+        >>> tk2 = Token('4', TokenType.NUM)
+        >>> parser = Parser([tk0, tk1, tk2])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        1
+
+        >>> tk0 = Token('4', TokenType.NUM)
+        >>> tk1 = Token('<', TokenType.LTH)
+        >>> tk2 = Token('4', TokenType.NUM)
+        >>> parser = Parser([tk0, tk1, tk2])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        0
+
+        >>> tk0 = Token('not', TokenType.NOT)
+        >>> tk1 = Token('4', TokenType.NUM)
+        >>> tk2 = Token('<', TokenType.LTH)
+        >>> tk3 = Token('4', TokenType.NUM)
+        >>> parser = Parser([tk0, tk1, tk2, tk3])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        1
+
+        >>> tk0 = Token('let', TokenType.LET)
+        >>> tk1 = Token('v', TokenType.VAR)
+        >>> tk2 = Token('<-', TokenType.ASN)
+        >>> tk3 = Token('42', TokenType.NUM)
+        >>> tk4 = Token('in', TokenType.INX)
+        >>> tk5 = Token('v', TokenType.VAR)
+        >>> tk6 = Token('end', TokenType.END)
+        >>> parser = Parser([tk0, tk1, tk2, tk3, tk4, tk5, tk6])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        42
+
+        >>> tk0 = Token('let', TokenType.LET)
+        >>> tk1 = Token('v', TokenType.VAR)
+        >>> tk2 = Token('<-', TokenType.ASN)
+        >>> tk3 = Token('21', TokenType.NUM)
+        >>> tk4 = Token('in', TokenType.INX)
+        >>> tk5 = Token('v', TokenType.VAR)
+        >>> tk6 = Token('+', TokenType.ADD)
+        >>> tk7 = Token('v', TokenType.VAR)
+        >>> tk8 = Token('end', TokenType.END)
+        >>> parser = Parser([tk0, tk1, tk2, tk3, tk4, tk5, tk6, tk7, tk8])
+        >>> g = GenVisitor()
+        >>> p = AsmModule.Program({}, [])
+        >>> exp = parser.parse()
+        >>> v = exp.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        42
+        """
+
+        expr = self._parse_expression()
+        self._expect(TokenType.EOF)
+        return expr
+
+    def _parse_expression(self):
+        return self._parse_equality()
+
+    def _parse_let(self):
+        self._expect(TokenType.LET)
+        identifier = self._expect(TokenType.VAR).text
+        self._expect(TokenType.ASN)
+        exp_def = self._parse_expression()
+        self._expect(TokenType.INX)
+        exp_body = self._parse_expression()
+        self._expect(TokenType.END)
+        return Let(identifier, exp_def, exp_body)
+
+    def _parse_equality(self):
+        left = self._parse_comparison()
+        while self._current().kind == TokenType.EQL:
+            self._advance()
+            right = self._parse_comparison()
+            left = Eql(left, right)
+        return left
+
+    def _parse_comparison(self):
+        left = self._parse_additive()
+        while True:
+            token_kind = self._current().kind
+            if token_kind == TokenType.LEQ:
+                self._advance()
+                right = self._parse_additive()
+                left = Leq(left, right)
+            elif token_kind == TokenType.LTH:
+                self._advance()
+                right = self._parse_additive()
+                left = Lth(left, right)
+            else:
+                break
+        return left
+
+    def _parse_additive(self):
+        left = self._parse_multiplicative()
+        while True:
+            token_kind = self._current().kind
+            if token_kind == TokenType.ADD:
+                self._advance()
+                right = self._parse_multiplicative()
+                left = Add(left, right)
+            elif token_kind == TokenType.SUB:
+                self._advance()
+                right = self._parse_multiplicative()
+                left = Sub(left, right)
+            else:
+                break
+        return left
+
+    def _parse_multiplicative(self):
+        left = self._parse_unary()
+        while True:
+            token_kind = self._current().kind
+            if token_kind == TokenType.MUL:
+                self._advance()
+                right = self._parse_unary()
+                left = Mul(left, right)
+            elif token_kind == TokenType.DIV:
+                self._advance()
+                right = self._parse_unary()
+                left = Div(left, right)
+            else:
+                break
+        return left
+
+    def _parse_unary(self):
+        token_kind = self._current().kind
+        if token_kind == TokenType.NEG:
+            self._advance()
+            return Neg(self._parse_unary())
+        if token_kind == TokenType.NOT:
+            self._advance()
+            return Not(self._parse_unary())
+        return self._parse_primary()
+
+    def _parse_primary(self):
+        token = self._current()
+        if token.kind == TokenType.NUM:
+            self._advance()
+            return Num(int(token.text))
+        if token.kind == TokenType.TRU:
+            self._advance()
+            return Bln(True)
+        if token.kind == TokenType.FLS:
+            self._advance()
+            return Bln(False)
+        if token.kind == TokenType.VAR:
+            self._advance()
+            return Var(token.text)
+        if token.kind == TokenType.LPR:
+            self._advance()
+            expr = self._parse_expression()
+            self._expect(TokenType.RPR)
+            return expr
+        if token.kind == TokenType.LET:
+            return self._parse_let()
+        self._error("Unexpected token")
+
+    def _current(self):
+        return self.tokens[self.cur_token_idx]
+
+    def _advance(self):
+        if self.cur_token_idx < len(self.tokens) - 1:
+            self.cur_token_idx += 1
+
+    def _expect(self, token_type):
+        token = self._current()
+        if token.kind != token_type:
+            self._error(f"Expected {token_type.name}")
+        self._advance()
+        return token
+
+    def _error(self, message):
+        sys.exit(f"Parse error: {message}")
---
+++ Vpl11/Visitor.py
@@ -0,0 +1,471 @@
+import sys
+from abc import ABC, abstractmethod
+from Expression import *
+import Asm as AsmModule
+
+
+class Visitor(ABC):
+    """
+    The visitor pattern consists of two abstract classes: the Expression and the
+    Visitor. The Expression class defines on method: 'accept(visitor, args)'.
+    This method takes in an implementation of a visitor, and the arguments that
+    are passed from expression to expression. The Visitor class defines one
+    specific method for each subclass of Expression. Each instance of such a
+    subclasse will invoke the right visiting method.
+    """
+
+    @abstractmethod
+    def visit_var(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_bln(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_num(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_eql(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_add(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_sub(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_mul(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_div(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_leq(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_lth(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_neg(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_not(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_let(self, exp, arg):
+        pass
+
+
+class GenVisitor(Visitor):
+    """
+    The GenVisitor class compiles arithmetic expressions into a low-level
+    language.
+    """
+
+    def __init__(self):
+        self.next_var_counter = 0
+
+    def next_var_name(self):
+        self.next_var_counter += 1
+        return f"tmp{self.next_var_counter}"
+
+    def visit_var(self, exp, prog):
+        """
+        Usage:
+            >>> e = Var('x')
+            >>> p = AsmModule.Program({"x":1}, [])
+            >>> g = GenVisitor()
+            >>> v = e.accept(g, p)
+            >>> p.eval()
+            >>> p.get_val(v)
+            1
+        """
+        return exp.identifier
+
+    def visit_bln(self, exp, env):
+        """
+        Usage:
+            >>> e = Bln(True)
+            >>> p = AsmModule.Program({}, [])
+            >>> g = GenVisitor()
+            >>> v = e.accept(g, p)
+            >>> p.eval()
+            >>> p.get_val(v)
+            1
+
+            >>> e = Bln(False)
+            >>> p = AsmModule.Program({}, [])
+            >>> g = GenVisitor()
+            >>> v = e.accept(g, p)
+            >>> p.eval()
+            >>> p.get_val(v)
+            0
+        """
+        dest = self.next_var_name()
+        value = 1 if exp.bln else 0
+        env.add_inst(AsmModule.Addi(dest, "x0", value))
+        return dest
+
+    def visit_num(self, exp, prog):
+        """
+        Usage:
+            >>> e = Num(13)
+            >>> p = AsmModule.Program({}, [])
+            >>> g = GenVisitor()
+            >>> v = e.accept(g, p)
+            >>> p.eval()
+            >>> p.get_val(v)
+            13
+        """
+        dest = self.next_var_name()
+        prog.add_inst(AsmModule.Addi(dest, "x0", exp.num))
+        return dest
+
+    def visit_eql(self, exp, prog):
+        """
+        >>> e = Eql(Num(13), Num(13))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        1
+
+        >>> e = Eql(Num(13), Num(10))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        0
+
+        >>> e = Eql(Num(-1), Num(1))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        0
+        """
+        left_var = exp.left.accept(self, prog)
+        right_var = exp.right.accept(self, prog)
+        diff = self.next_var_name()
+        prog.add_inst(AsmModule.Sub(diff, left_var, right_var))
+        lt_one = self.next_var_name()
+        prog.add_inst(AsmModule.Slti(lt_one, diff, 1))
+        lt_zero = self.next_var_name()
+        prog.add_inst(AsmModule.Slti(lt_zero, diff, 0))
+        result = self.next_var_name()
+        prog.add_inst(AsmModule.Xor(result, lt_one, lt_zero))
+        return result
+
+    def visit_add(self, exp, prog):
+        """
+        >>> e = Add(Num(13), Num(-13))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        0
+
+        >>> e = Add(Num(13), Num(10))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        23
+        """
+        left_var = exp.left.accept(self, prog)
+        right_var = exp.right.accept(self, prog)
+        dest = self.next_var_name()
+        prog.add_inst(AsmModule.Add(dest, left_var, right_var))
+        return dest
+
+    def visit_sub(self, exp, prog):
+        """
+        >>> e = Sub(Num(13), Num(-13))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        26
+
+        >>> e = Sub(Num(13), Num(10))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        3
+        """
+        left_var = exp.left.accept(self, prog)
+        right_var = exp.right.accept(self, prog)
+        dest = self.next_var_name()
+        prog.add_inst(AsmModule.Sub(dest, left_var, right_var))
+        return dest
+
+    def visit_mul(self, exp, prog):
+        """
+        >>> e = Mul(Num(13), Num(2))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        26
+
+        >>> e = Mul(Num(13), Num(10))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        130
+        """
+        left_var = exp.left.accept(self, prog)
+        right_var = exp.right.accept(self, prog)
+        dest = self.next_var_name()
+        prog.add_inst(AsmModule.Mul(dest, left_var, right_var))
+        return dest
+
+    def visit_div(self, exp, prog):
+        """
+        >>> e = Div(Num(13), Num(2))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        6
+
+        >>> e = Div(Num(13), Num(10))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        1
+        """
+        left_var = exp.left.accept(self, prog)
+        right_var = exp.right.accept(self, prog)
+        dest = self.next_var_name()
+        prog.add_inst(AsmModule.Div(dest, left_var, right_var))
+        return dest
+
+    def visit_leq(self, exp, prog):
+        """
+        >>> e = Leq(Num(3), Num(2))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        0
+
+        >>> e = Leq(Num(3), Num(3))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        1
+
+        >>> e = Leq(Num(2), Num(3))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        1
+
+        >>> e = Leq(Num(-3), Num(-2))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        1
+
+        >>> e = Leq(Num(-3), Num(-3))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        1
+
+        >>> e = Leq(Num(-2), Num(-3))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        0
+        """
+        left_var = exp.left.accept(self, prog)
+        right_var = exp.right.accept(self, prog)
+        lower_than = self.next_var_name()
+        prog.add_inst(AsmModule.Slt(lower_than, right_var, left_var))
+        dest = self.next_var_name()
+        prog.add_inst(AsmModule.Xori(dest, lower_than, 1))
+        return dest
+
+    def visit_lth(self, exp, prog):
+        """
+        >>> e = Lth(Num(3), Num(2))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        0
+
+        >>> e = Lth(Num(3), Num(3))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        0
+
+        >>> e = Lth(Num(2), Num(3))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        1
+        """
+        left_var = exp.left.accept(self, prog)
+        right_var = exp.right.accept(self, prog)
+        dest = self.next_var_name()
+        prog.add_inst(AsmModule.Slt(dest, left_var, right_var))
+        return dest
+
+    def visit_neg(self, exp, prog):
+        """
+        >>> e = Neg(Num(3))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        -3
+
+        >>> e = Neg(Num(0))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        0
+
+        >>> e = Neg(Num(-3))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        3
+        """
+        operand = exp.exp.accept(self, prog)
+        dest = self.next_var_name()
+        prog.add_inst(AsmModule.Sub(dest, "x0", operand))
+        return dest
+
+    def visit_not(self, exp, prog):
+        """
+        >>> e = Not(Bln(True))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        0
+
+        >>> e = Not(Bln(False))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        1
+
+        >>> e = Not(Num(0))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        1
+
+        >>> e = Not(Num(-2))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        0
+
+        >>> e = Not(Num(2))
+        >>> p = AsmModule.Program({}, [])
+        >>> g = GenVisitor()
+        >>> v = e.accept(g, p)
+        >>> p.eval()
+        >>> p.get_val(v)
+        0
+        """
+        operand = exp.exp.accept(self, prog)
+        lt_one = self.next_var_name()
+        prog.add_inst(AsmModule.Slti(lt_one, operand, 1))
+        lt_zero = self.next_var_name()
+        prog.add_inst(AsmModule.Slti(lt_zero, operand, 0))
+        dest = self.next_var_name()
+        prog.add_inst(AsmModule.Xor(dest, lt_one, lt_zero))
+        return dest
+
+    def visit_let(self, exp, prog):
+        """
+        Usage:
+            >>> e = Let('v', Not(Bln(False)), Var('v'))
+            >>> p = AsmModule.Program({}, [])
+            >>> g = GenVisitor()
+            >>> v = e.accept(g, p)
+            >>> p.eval()
+            >>> p.get_val(v)
+            1
+
+            >>> e = Let('v', Num(2), Add(Var('v'), Num(3)))
+            >>> p = AsmModule.Program({}, [])
+            >>> g = GenVisitor()
+            >>> v = e.accept(g, p)
+            >>> p.eval()
+            >>> p.get_val(v)
+            5
+
+            >>> e0 = Let('x', Num(2), Add(Var('x'), Num(3)))
+            >>> e1 = Let('y', e0, Mul(Var('y'), Num(10)))
+            >>> p = AsmModule.Program({}, [])
+            >>> g = GenVisitor()
+            >>> v = e1.accept(g, p)
+            >>> p.eval()
+            >>> p.get_val(v)
+            50
+        """
+        value_var = exp.exp_def.accept(self, prog)
+        prog.add_inst(AsmModule.Add(exp.identifier, value_var, "x0"))
+        return exp.exp_body.accept(self, prog)
